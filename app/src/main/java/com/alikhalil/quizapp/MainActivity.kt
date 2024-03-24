ee28f25cf7ec38d1e1931a082eccfdaf898093cf
Ali Alaql <23135660+alikhleel@users.noreply.github.com>
2024-03-25 00:32:09 +0300

    try adding filter chips

--- app/src/main/java/com/alikhalil/quizapp/MainActivity.kt
+++ app/src/main/java/com/alikhalil/quizapp/MainActivity.kt
@@ -10,20 +10,30 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.alikhalil.quizapp.ui.theme.QuizAppTheme
 
@@ -39,6 +49,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Column(Modifier.padding(16.dp)) {
+//                        Quiz()
+                        FilterChipExample()
                         Quiz()
                     }
                 }
@@ -47,13 +59,59 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FilterChipExample() {
+    val categories = listOf(
+        "Apartment",
+        "House",
+        "Villa",
+        "Office",
+        "Shop",
+        "Land",
+    )
+    var selectedCategories by remember { mutableStateOf(emptyList<String>()) }
+    LazyRow(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(categories) { category ->
+            FilterChip(
+                onClick = {
+                    selectedCategories = if (category in selectedCategories) {
+                        selectedCategories - category
+                    } else {
+                        selectedCategories + category
+                    }
+                },
+                label = {
+                    Text(text = category, fontSize = 24.sp)
+                },
+                selected = category in selectedCategories,
+                leadingIcon = if (category in selectedCategories) {
+                    {
+                        Icon(
+                            Icons.Default.Done,
+                            contentDescription = null,
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
+                } else {
+                    null
+                },
+            )
+        }
+    }
+}
+
+
 @Composable
 fun Quiz(
     viewModel: QuizViewModel = viewModel()
 ) {
     LazyColumn {
         items(viewModel.quizList) { quizItem ->
-            val selectedOption = rememberSaveable    { mutableStateOf("") }
+            val selectedOption = rememberSaveable { mutableStateOf("") }
             Text(text = quizItem.question)
             quizItem.answersList.forEach { answer ->
                 Row(
@@ -82,7 +140,7 @@ fun Quiz(
             }
 
         }
-        item{
+        item {
             Text(text = viewModel.score.value)
         }
     }
